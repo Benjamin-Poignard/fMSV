4a163ed95d4dccce36f1fdf536531921a80e2754
Benjamin-Poignard <104049948+Benjamin-Poignard@users.noreply.github.com>
2025-02-10 10:23:32 +0900

    Delete DM.m

--- DM.m
+++
@@ -1,8 +0,0 @@
-function [alpha,se_a,t_value] = DM(vE)
-  iF = length(vE);
-  alpha = mean(vE);
-  iV = HAC(vE-alpha,ones(iF,1))/(iF);
-  se_a = sqrt(iV);
-  t_value = alpha/se_a;
-  %[alpha se_a t_value]
-end
