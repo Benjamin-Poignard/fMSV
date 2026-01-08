4911bdcf0ea91890a09ad3f329f00f42d1ffdf08
Benjamin-Poignard <104049948+Benjamin-Poignard@users.noreply.github.com>
2026-01-08 13:36:39 +0900

    Delete fNCov.m

--- fNCov.m
+++
@@ -1,8 +0,0 @@
-function mC = fNCov(mA)
-  % nearest covariance matrix
-  [mP,mE] = eig(mA);
-  vE = diag(mE);iM=length(vE);
-  vEs = vE.*(vE>0) + (1e-4)*ones(iM,1).*(vE<=0);
-  mC = mP*diag(vEs)*mP';
-  %mC = mP*abs(mE)*mP';
-end
